83d53532e7b520509d61c579df8287704be82daf
Milo Watanabe <milo.wata@gmail.com>
2014-02-03 15:46:40 -0500

    Modifications for heroku hosting

--- abescheme/settings.py
+++ abescheme/settings.py
@@ -1,5 +1,8 @@
 # Django settings for abescheme project.
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -7,18 +10,22 @@ ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 MANAGERS = ADMINS
 
-DATABASES = {
+if os.environ.get('DATABASE_URL', None):
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': '/Users/Milo/Documents/Classes/Thesis/abescheme/privacy.db',
     }
 }
-
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -52,21 +59,11 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -76,6 +73,9 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'a=-k3tdab7rksuncfhy0hi#f21_iotr7@c=zx_p6(d5wk0t5_c'
